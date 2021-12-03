b9441fea71e1555f774ebc36203d13e9768b5315
agurchand <agurchand@gmail.com>
2021-12-03 12:24:39 +0530

    init commit

---
+++ data.php
@@ -0,0 +1,32 @@
+<?php
+//connect to database
+include('../db_con.php');
+$id = $_REQUEST['id'];
+
+$arr 	= array();
+$arr1 	= array();
+$result = array();
+
+$sql = "select * from higcharts_data where map_id = $id";
+$q	 = $mysqli -> query($sql);
+
+$j = 0;
+while($row = $q->fetch_assoc()){
+
+	if($j == 0){
+	$arr['name'] = 'Male';
+	$arr1['name'] = 'Female';
+		$j++;
+	}
+	$arr['data'][] = $row['male'];
+	$arr1['data'][] = $row['female'];
+}
+	
+array_push($result,$arr);
+array_push($result,$arr1);
+
+print json_encode($result, JSON_NUMERIC_CHECK);
+
+$mysqli -> close();
+
+?>
